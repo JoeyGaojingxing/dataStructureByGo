44019dbc47099e545c2846532999d9b7489f9fa1
Mojerro <mojerro@126.com>
2019-05-10 07:57:39 +0800

    合法取值在0-1的链表

--- Basic/LinkedList.go
+++ Basic/LinkedList.go
@@ -1,8 +1,6 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 type MyLinkedList struct {
 	data int
@@ -19,6 +17,9 @@ func (this *MyLinkedList) GetPoint(index int) *MyLinkedList {
 	for i := 0; i <= index; i++ {
 		if i == index {
 			// this is the value
+			if this.data == 0 {
+				return nil
+			}
 			return this
 		} else if this.Next == nil {
 			// if end
@@ -34,7 +35,7 @@ func (this *MyLinkedList) GetPoint(index int) *MyLinkedList {
 /** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
 func (this *MyLinkedList) Get(index int) int {
 	val := this.GetPoint(index)
-	if val == nil || val.data == 0 {
+	if val == nil {
 		return -1
 	} else {
 		return val.data
@@ -43,25 +44,31 @@ func (this *MyLinkedList) Get(index int) int {
 
 /** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
 func (this *MyLinkedList) AddAtHead(val int) {
-	pre := this
-	if pre.data != 0 {
-		pre := MyLinkedList{
-			data: this.data,
-			Next: this.Next,
-		}
+	if val == 0 {
+		return
+	}
+	if this.data == 0 {
 		this.data = val
-		this.Next = &pre
 	} else {
+		list := &MyLinkedList{data: this.data, Next: this.Next}
 		this.data = val
+		this.Next = list
 	}
 }
 
 /** Append a node of value val to the last element of the linked list. */
 func (this *MyLinkedList) AddAtTail(val int) {
-	for i := 0; ; i++ {
-		if this.Next == nil {
-			next := MyLinkedList{data: val}
-			this.Next = &next
+	if val == 0 {
+		return
+	}
+	for {
+		if this.data == 0 {
+			this.data = val
+			break
+		} else if this.Next == nil {
+			tail := &MyLinkedList{data: val}
+			tail.data = val
+			this.Next = tail
 			break
 		} else {
 			this = this.Next
@@ -71,49 +78,34 @@ func (this *MyLinkedList) AddAtTail(val int) {
 
 /** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
 func (this *MyLinkedList) AddAtIndex(index int, val int) {
-	var pre *MyLinkedList
+	if val == 0 {
+		return
+	}
+	pre := this
 	for i := 0; ; i++ {
 		if index == 0 {
 			this.AddAtHead(val)
 			break
-		} else if this.Next == nil {
-			if this.data == 0 && i == 0 {
-				break
-			} else if i == index && pre != nil {
-				insert := MyLinkedList{data: val, Next: this}
-				pre.Next = &insert
-				break
-			} else if i == index+1 {
-				this.AddAtTail(val)
-				break
-			} else {
-				continue
-			}
-		} else if i != index {
+		} else if i == index {
+			next := &MyLinkedList{data: val, Next: this}
+			pre.Next = next
+			break
+		} else if this != nil && this.data != 0 {
 			pre = this
 			this = this.Next
-		} else if i == index {
-			insert := MyLinkedList{data: val, Next: pre.Next}
-			pre.Next = &insert
-			break
 		} else {
-			// raise wrong info
 			break
 		}
 	}
-	_ = pre
 }
 
 /** Delete the index-th node in the linked list, if the index is valid. */
 func (this *MyLinkedList) DeleteAtIndex(index int) {
-	data := this.GetPoint(index - 1)
-	if data != nil {
-		next := this.GetPoint(index + 1)
-		if next != nil {
-			data.Next = next
-		}
+	pre := this.GetPoint(index - 1)
+	next := this.GetPoint(index + 1)
+	if pre != nil {
+		pre.Next = next
 	}
-
 }
 
 /**
@@ -125,21 +117,33 @@ func (this *MyLinkedList) DeleteAtIndex(index int) {
  * obj.AddAtIndex(index,val);
  * obj.DeleteAtIndex(index);
  */
+
+//func main() {
+//	obj := Constructor()
+//	obj.AddAtHead(0)
+//	obj.AddAtIndex(1, 9)
+//	obj.AddAtIndex(1, 5)
+//	obj.AddAtTail(7)
+//	obj.AddAtHead(1)
+//	obj.AddAtIndex(5, 8)
+//	obj.AddAtIndex(5, 2)
+//	obj.AddAtIndex(3, 0)
+//	for i:=0; i<10; i++ {
+//		fmt.Print(obj.Get(i))
+//	}
+//	obj.AddAtTail(1)
+//	obj.AddAtTail(0)
+//	obj.DeleteAtIndex(6)
+//	fmt.Println("end", obj.Get(3))
+//}
 func main() {
-	val, index := 23, 1
 	obj := Constructor()
-	param1 := obj.Get(index)
-	fmt.Println("0=>", param1) // -1
-	obj.AddAtHead(val - 1)
-	obj.AddAtHead(val - 9)
-	fmt.Println("1=>", obj.Get(0)) // 23
-	obj.AddAtTail(val + 1)
-	fmt.Println("2=>", obj.Get(1))
-	fmt.Println("result=>", obj.Get(0), obj.Get(1))
-	obj.AddAtIndex(1, val)
-	fmt.Println("3=>", obj.Get(index))
-	fmt.Println("result=>", obj.Get(0), obj.Get(1), obj.Get(2))
-	//obj.DeleteAtIndex(index)
-	//fmt.Println("4=>", obj.Get(index))
-	fmt.Println("result=>", obj.Get(0), obj.Get(1), obj.Get(2), obj.Get(3))
+	for i := 0; i < 5; i++ {
+		obj.AddAtTail(i)
+		obj.AddAtHead(i)
+		obj.AddAtIndex(5, i)
+	}
+	for i := 0; i < 10; i++ {
+		fmt.Print(" |", obj.Get(i))
+	}
 }
