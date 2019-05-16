f4d41e14d72f75164331193e795be6dc89d60cb4
Mojerro <mojerro@126.com>
2019-05-16 13:25:24 +0800

    添加输入校验

--- src/main/JosephusCircleArr.go
+++ src/main/JosephusCircleArr.go
@@ -22,12 +22,14 @@ func main() {
 	)
 	fmt.Println("约瑟夫环问题\n请输入人数 报数周期 报数起始人")
 	n, _ = fmt.Scanln(&length, &times, &starter)
-	if length == 0 || n == 0 {
-		fmt.Println("数组长度有误")
+	if length <= 0 || n == 0 {
+		fmt.Println("人数有误")
 		return
-	} else if starter >= length {
+	} else if starter >= length || starter <= 0 {
 		fmt.Println("起始位置有误")
 		return
+	} else if times <= 0 {
+		fmt.Println("报数周期有误")
 	}
 	// generate people array
 	var arr []int
--- src/main/JosephusCircleLinkedList.go
+++ src/main/JosephusCircleLinkedList.go
@@ -23,15 +23,17 @@ func main() {
 	)
 	fmt.Println("约瑟夫环问题\n请输入人数 报数周期 报数起始人")
 	n, _ = fmt.Scanln(&length, &times, &starter)
-	if length == 0 || n == 0 {
+	if length <= 0 || n == 0 {
 		fmt.Println("数组长度有误")
 		return
 	} else if length > 1000 {
 		fmt.Println("最大长度为1000")
 		return
-	} else if starter >= length {
+	} else if starter >= length || starter <= 0 {
 		fmt.Println("起始位置有误")
 		return
+	} else if times <= 0 {
+		fmt.Println("报数周期有误")
 	}
 	// generate people array
 	//linkList := structure.Constructor()
